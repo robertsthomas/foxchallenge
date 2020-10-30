659db2ec39099557ef778a2bede305d571d91ce7
Thomas Roberts <robertsthomasd@gmail.com>
2020-10-29 20:47:03 -0700

    add alt to img

--- src/App.js
+++ src/App.js
@@ -314,6 +314,7 @@ function App() {
             }}
           >
             <img
+              alt="test"
               src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
               style={{
                 objectFit: "cover",
